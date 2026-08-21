d186a23671bcb467e85a971db0cd4d9b4e0df497
mariokreitz <mario.kreitz@web.de>
2026-08-21 13:00:10 +0200

    feat(server): configure allowed hosts for Angular app

--- apps/portal/src/server.ts
+++ apps/portal/src/server.ts
@@ -12,7 +12,9 @@ const serverDistFolder = dirname(fileURLToPath(import.meta.url));
 const browserDistFolder = resolve(serverDistFolder, '../browser');
 
 const app = express();
-const angularApp = new AngularNodeAppEngine();
+const angularApp = new AngularNodeAppEngine({
+  allowedHosts: ['http://localhost:4201/'],
+});
 
 app.use(
   express.static(browserDistFolder, {
