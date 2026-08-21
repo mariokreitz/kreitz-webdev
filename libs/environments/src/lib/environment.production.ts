4ab147760655cc5398b03b4f7409ad74c188ac5b
mariokreitz <mario.kreitz@web.de>
2026-08-21 13:34:12 +0200

    fix(environment): correct authBaseUrl format

--- libs/environments/src/lib/environment.production.ts
+++ libs/environments/src/lib/environment.production.ts
@@ -3,7 +3,7 @@ import type { Environment } from './types/environment';
 export const environment: Environment = {
   production: true,
   api: {
-    authBaseUrl: 'https://api.kreitz-webdev.de/',
+    authBaseUrl: 'https://api.kreitz-webdev.de',
     kreitzWebdev: 'https://api.kreitz-webdev.de/api',
   },
 };
