c5640f0e593c994a16cb8908067111fb8351c69e
mariokreitz <mario.kreitz@web.de>
2026-08-23 05:34:10 +0200

    feat(prisma): adds new db schema.prisma models related to users website and project

--- apps/api/prisma/migrations/20260821064059_add_account_issuer/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "account" ADD COLUMN     "issuer" TEXT NOT NULL;
-
--- CreateIndex
-CREATE UNIQUE INDEX "account_issuer_accountId_uidx" ON "account"("issuer", "accountId");
